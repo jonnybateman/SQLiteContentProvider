7c58c39b4806be8a650aaffd27d96d0d95a8b81c
Jonathan Bateman <52790777+jonnybateman@users.noreply.github.com>
2022-02-09 15:23:42 +0100

    Update SQLiteContentProvider.java

--- SQLiteContentProvider.java
+++ SQLiteContentProvider.java
@@ -1,7 +1,7 @@
 /*------------------------------------------------------------------------------------------
  |              Class: SQLiteContentProvider.java
  |             Author: Jon Bateman
- |            Version: 1.2.3
+ |            Version: 1.2.4
  |
  |            Purpose: Content Provider used for interacting with a SQLite database. Targeted
  |                     database name is passed as a URI parameter so that the relevant DBHelper
@@ -14,7 +14,7 @@
  |
  |                     App specific alterations:-
  |                         1. At line 26 use package name specific to your app.
- |                         2. At line 65 enter name of your provider authority. This should be
+ |                         2. At line 67 enter name of your provider authority. This should be
  |                            in internet domain ownership format, e.g. com.abc.xyz
  |
  |      Inherits from: ContentProvider.class
@@ -23,7 +23,7 @@
  |
  | Intent/Bundle Args: N/A
  +------------------------------------------------------------------------------------------*/
-package <app package name>;
+package <package_name>;
 
 import android.content.ContentProvider;
 import android.content.ContentProviderOperation;
@@ -38,6 +38,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.CancellationSignal;
 import android.util.Base64;
 import android.util.Log;
 import java.io.File;
@@ -61,11 +62,7 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class SQLiteContentProvider extends ContentProvider {
 
-    // This is the symbolic name of your provider. To avoid conflicts with other providers, you
-    // should use internet domain ownership as the basis of your provider authority.
     private static final String AUTHORITY = "<provider.authority.name>";
-    // The content authority is used to create the base of all URIs which apps will use to contact
-    // this content provider.
     private static final Uri BASE_URI = Uri.parse("content://" + AUTHORITY);
 
     private static final String PATH_DML_STATEMENT = "dml_statement";
@@ -75,7 +72,6 @@ public class SQLiteContentProvider extends ContentProvider {
     private static final String PATH_APPLY_BATCH = "apply_batch";
     private static final String PATH_FK_CONSTRAINT = "fk_constraint";
 
-    // Use an int for each URI we will run, this represents the different database actions.
     private static final int DML_STATEMENT = 2;
     private static final int DDL_STATEMENT = 6;
     private static final int SIMPLE_QUERY = 3;
@@ -83,7 +79,6 @@ public class SQLiteContentProvider extends ContentProvider {
     private static final int APPLY_BATCH = 5;
     private static final int FK_CONSTRAINT = 7;
 
-    // Key constants used to identify parameters within the URI.
     private static final String KEY_URI_PARAMETER_DATABASE = "database";
     private static final String KEY_URI_PARAMETER_TABLE = "table";
     private static final String KEY_URI_PARAMETER_SQL = "sql";
@@ -91,14 +86,10 @@ public class SQLiteContentProvider extends ContentProvider {
     private static final String KEY_URI_PARAMETER_PROVIDER_ACCESS_CODE = "access_code";
     private static final String KEY_URI_PARAMETER_FK = "foreign_key";
 
-    // Key constant used to identify/tag the result of a non-query action when assigning it to a Bundle.
     private static final String KEY_CURSOR_RESULT = "result";
 
-    // HashMap used to store DBHelper instances, one for each database. Each instance identified by
-    // it's database name.
     private DBHelper dbHelper;
 
-    // A URIMatcher that will take in a URI and match it to the appropriate integer identifier above.
     private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 
     static {
@@ -116,10 +107,8 @@ public class SQLiteContentProvider extends ContentProvider {
         return true;
     }
 
-    // Create a SQLiteOpenHelper class instance which is used to access the targeted database.
     private static class DBHelper extends SQLiteOpenHelper {
 
-        // Constructor
         DBHelper(Context context, String dbName, int dbVersion) {
             super(context, dbName, null, dbVersion);
         }
@@ -134,7 +123,6 @@ public class SQLiteContentProvider extends ContentProvider {
             // Database not to be upgraded through content provider.
         }
 
-        // Enable foreign key constraints in database
         @Override
         public void onConfigure(SQLiteDatabase db) {
             super.onConfigure(db);
@@ -144,32 +132,25 @@ public class SQLiteContentProvider extends ContentProvider {
         }
     }
 
-    // The getType() method is used to find the MIME type of the results, either a directory of
-    // multiple records, or an individual record.
     @Override
     public String getType(Uri uri) {
         return null;
     }
 
-    // Parameters:
-    //  uri: The URI to identify the database to be targeted as well as the action to be taken against it.
-    //  projection: A string array of columns that will be returned in the result set.
-    //  selection: A string defining the criteria for results to be returned.
-    //  selectionArgs: Arguments to the above criteria that rows will be checked against.
-    //  sortOrder: A string of the column(s) and order to sort the results.
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return null;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder, CancellationSignal cancelSignal) {
 
         Cursor cursor = null;
 
-        // Determine if the incoming URI request is from SQLiteDevStudio.
         if (decryptUriAccessParameter(uri.getQueryParameter(KEY_URI_PARAMETER_PROVIDER_ACCESS_CODE))) {
 
-            // Get the name of the targeted database.
             String dbName = uri.getQueryParameter(KEY_URI_PARAMETER_DATABASE);
 
-            // Create a new database helper instance if one has not yet been created or if the targeted
-            // database has changed.
             if (dbHelper == null || !dbHelper.getDatabaseName().equals(dbName)) {
                 createDBHelperInstance(uri);
             }
@@ -177,24 +158,20 @@ public class SQLiteContentProvider extends ContentProvider {
             if (dbHelper != null) {
                 final SQLiteDatabase db = dbHelper.getWritableDatabase();
 
-                // Get any parameters that may be attached to the URI.
                 String table = uri.getQueryParameter(KEY_URI_PARAMETER_TABLE);
                 String sql = uri.getQueryParameter(KEY_URI_PARAMETER_SQL);
                 String rowsLimit = uri.getQueryParameter(KEY_URI_PARAMETER_LIMIT);
 
                 if (sql != null) {
-                    // Append sort order to SQL string if has been supplied.
                     if (sortOrder != null) {
                         sql = sql.concat(" order by " + sortOrder);
                     }
 
-                    // Append row limit to SQL string if it has been supplied.
                     if (rowsLimit != null) {
                         sql = sql.concat(" LIMIT " + rowsLimit);
                     }
                 } else {
                     if (rowsLimit != null) {
-                        // No SQL string so apply row limit to sortOrder parameter.
                         sortOrder = sortOrder + " LIMIT " + rowsLimit;
                     }
                 }
@@ -215,18 +192,14 @@ public class SQLiteContentProvider extends ContentProvider {
 
                     case DML_STATEMENT:
                     case DDL_STATEMENT:
-                        // Initialise cursor. Doing it this way allows for a Bundle to be returned with the cursor
-                        // for a SQL statement that does not retrieve a data set.
                         cursor = db.rawQuery("select 1 from sqlite_master limit 1", null);
 
-                        // Create a bundle to store the result of a non query database action.
                         Bundle returnBundle = new Bundle();
 
                         db.execSQL(sql);
                         returnBundle.putInt(KEY_CURSOR_RESULT, 0);
 
                         if (cursor != null) {
-                            // Assign bundle to cursor to pass back result.
                             cursor.setExtras(returnBundle);
                         }
 
@@ -234,20 +207,16 @@ public class SQLiteContentProvider extends ContentProvider {
 
                     case COMPLEX_QUERY:
                         if (sql != null) {
-                            cursor = db.rawQuery(sql, selectionArgs);
+                            cursor = db.rawQuery(sql, selectionArgs, cancelSignal);
                         }
 
                         break;
 
                     case FK_CONSTRAINT:
-                        // Initialise cursor. Doing it this way allows for a Bundle to be returned with the cursor
-                        // for a SQL statement that does not retrieve a data set.
                         cursor = db.rawQuery("select 1 from sqlite_master limit 1", null);
 
-                        // Create a bundle to store the result of a non query database action.
                         returnBundle = new Bundle();
 
-                        // Toggle the foreign key constraints.
                         String toggleFkConstraint = uri.getQueryParameter(KEY_URI_PARAMETER_FK);
                         if (toggleFkConstraint != null)
                             db.setForeignKeyConstraintsEnabled(Boolean.parseBoolean(toggleFkConstraint));
@@ -255,7 +224,6 @@ public class SQLiteContentProvider extends ContentProvider {
                         returnBundle.putInt(KEY_CURSOR_RESULT, 0);
 
                         if (cursor != null)
-                            // Assign bundle to cursor to pass back result.
                             cursor.setExtras(returnBundle);
 
                         break;
@@ -272,33 +240,24 @@ public class SQLiteContentProvider extends ContentProvider {
         return cursor;
     }
 
-    // The insert method takes in a ContentValues object, which is a key value pair of column names
-    // and values to be inserted.
     @Override
     public Uri insert(Uri uri, ContentValues contentValues) {
 
-        // Determine if the incoming URI request is from SQLiteDevStudio.
         if (decryptUriAccessParameter(uri.getQueryParameter(KEY_URI_PARAMETER_PROVIDER_ACCESS_CODE))) {
 
             Uri returnUri = null;
             long id = 0L;
 
             if (dbHelper != null) {
-                // Open the database readying it for DML/DDL operations.
                 final SQLiteDatabase db = dbHelper.getWritableDatabase();
 
-                // Get the table that is to be inserted into from the URI.
                 String table = uri.getQueryParameter(KEY_URI_PARAMETER_TABLE);
 
-                // -1 error, unable to insert record; >0 record successfully inserted returning id of new record.
-                // Any exception raised will be thrown to calling app.
                 id = db.insertOrThrow(table, null, contentValues);
 
-                // Create return URI for returning id to calling application.
                 returnUri = BASE_URI.buildUpon().appendPath(table).build();
             }
 
-            // Add id to the returned URI
             return ContentUris.withAppendedId(returnUri, id);
 
         } else {
@@ -306,26 +265,20 @@ public class SQLiteContentProvider extends ContentProvider {
         }
     }
 
-    // The update and delete methods take in a selection string and arguments to define which rows
-    // should be updated or deleted. Update method requires a ContentProvider object as well, for
-    // columns that will be updated.
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
 
         int rowsDeleted = 0;
 
-        // Determine if the incoming URI request is from SQLiteDevStudio.
         if (decryptUriAccessParameter(uri.getQueryParameter(KEY_URI_PARAMETER_PROVIDER_ACCESS_CODE))) {
 
             if (dbHelper != null) {
 
-                // Open the database readying it for DML/DDL operations.
                 final SQLiteDatabase db = dbHelper.getWritableDatabase();
 
-                // Get the table from which the record is to be deleted.
                 String table = uri.getQueryParameter(KEY_URI_PARAMETER_TABLE);
 
-                rowsDeleted = db.delete(table, selection, selectionArgs); // returns number of rows deleted.
+                rowsDeleted = db.delete(table, selection, selectionArgs);
             }
         }
 
@@ -337,19 +290,14 @@ public class SQLiteContentProvider extends ContentProvider {
 
         int rowsUpdated = 0;
 
-        // Determine if the incoming URI request is from SQLiteDevStudio.
         if (decryptUriAccessParameter(uri.getQueryParameter(KEY_URI_PARAMETER_PROVIDER_ACCESS_CODE))) {
 
             if (dbHelper != null) {
-
-                // Open the database readying it for DML/DDL operations.
                 final SQLiteDatabase db = dbHelper.getWritableDatabase();
 
-                // Get the table to be updated from the URI.
                 String table = uri.getQueryParameter(KEY_URI_PARAMETER_TABLE);
 
-                // Any exception will be raised to the calling app.
-                rowsUpdated = db.update(table, values, selection, selectionArgs); // return number of rows updated.
+                rowsUpdated = db.update(table, values, selection, selectionArgs);
             }
         }
 
@@ -361,13 +309,10 @@ public class SQLiteContentProvider extends ContentProvider {
 
         ContentProviderResult[] results = null;
 
-        // Determine if the incoming URI request is from SQLiteDevStudio.
         if (decryptUriAccessParameter(operations.get(0).getUri().getQueryParameter(KEY_URI_PARAMETER_PROVIDER_ACCESS_CODE))) {
             results = new ContentProviderResult[operations.size()];
 
             if (dbHelper != null) {
-
-                // Open database for writing to.
                 SQLiteDatabase db = dbHelper.getWritableDatabase();
 
                 db.beginTransaction();
@@ -387,13 +332,8 @@ public class SQLiteContentProvider extends ContentProvider {
         return results;
     }
 
-    /*
-     * Check if the database name passed in the uri from SQLiteDevStudio exists, if so create a
-     * DBHelper instance for the named database.
-     */
     private void createDBHelperInstance(Uri uri) {
 
-        // Get the database name from the uri.
         String dbName = uri.getQueryParameter(KEY_URI_PARAMETER_DATABASE);
 
         String dbPath = Objects.requireNonNull(getContext()).getApplicationInfo().dataDir + "/databases/";
@@ -425,42 +365,29 @@ public class SQLiteContentProvider extends ContentProvider {
         }
     }
 
-    /*
-     * A decrypt method that decrypts a passed uri parameter. If the decrypted value matches value
-     * stored in resource file then permit access to provider.
-     */
     private boolean decryptUriAccessParameter(String encodedEncryptedParameterString) {
 
         boolean accessAllowed = false;
 
         try {
-
-            // Decode the encrypted access parameter string to reveal the true encrypted string and convert
-            // it to a byte array variable.
             byte[] decodedEncryptedParameterByte = Base64.decode(encodedEncryptedParameterString, Base64.URL_SAFE);
 
-            // Create a key spec from the provided encryption key, used to decrypt the provider access parameter.
             SecretKeySpec sKeySpec = new SecretKeySpec(Objects.requireNonNull(
                     getContext()).getResources().getString(R.string.provider_encryption_key).getBytes()
                     , "AES");
 
-            // Initialise the Cipher for decrypting.
             Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
             cipher.init(Cipher.DECRYPT_MODE, sKeySpec, new GCMParameterSpec(
                     128, decodedEncryptedParameterByte, 0, 12));
 
-            // Use the Cipher to decrypt the provider access code.
             byte[] decodedDecryptedParameterByte = cipher.doFinal(
                     decodedEncryptedParameterByte, 12, decodedEncryptedParameterByte.length - 12);
 
-            // If the decrypted provider access code matches that stored then allow access to provider methods.
             if (new String(decodedDecryptedParameterByte, StandardCharsets.UTF_8)
                     .equals(getContext().getResources().getString(R.string.provider_access_code))) {
                 accessAllowed = true;
             }
 
-            // SecretKeySpec.destroy() method is an optional method. Not all implementations of SecretKey
-            // implement it.
             try {
                 Method method = SecretKeySpec.class.getMethod("destroy");
                 method.invoke(sKeySpec);
@@ -479,8 +406,6 @@ public class SQLiteContentProvider extends ContentProvider {
                 BadPaddingException |
                 IllegalBlockSizeException e) {
 
-            // Wrap the caught exception in a SecurityException. This will allow the exception to be
-            // thrown to the calling application for handling.
             throw new SecurityException(e);
         }
 
